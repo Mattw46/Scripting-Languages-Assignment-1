a42e1068958170b90a791ac6db158b7320071a4d
Mattw46 <matthew_w46@yahoo.com.au>
2015-04-11 11:07:41 +1000

    fourth commit

--- A1.pl
+++ A1.pl
@@ -5,23 +5,33 @@ use warnings;
 
 my @fileContents;
 
-# sub routines start here
+###########################
+# sub routines start here #
+###########################
 
 sub process {
  foreach (@fileContents) {
     # need to process the text here calling sub routines to process text
-    print $_;
+    #print $_;
 
     #while ($_ =~ /##/) { # add other characters in OR clause # assumes only one header per line
-    if ($_ =~ /###/) { # consider split out to new sub if too long
-      $_ = &transformH3($_);
+    
+    ###############################################################
+    #if ($_ =~ /###/) { # consider split out to new sub if too long
+    #  $_ = &transformH3($_);
+    #}
+    #elsif (($_ =~ /##/) && !($_ =~ /###/)) {
+    #  $_ = &transformH2($_);
+    #}
+    #elsif (($_ =~ /#/) && !($_ =~ /##/)) {
+    #  $_ = &transformH1($_);
+    #}
+    #############################################################
+    
+    if ($_ =~ /#/) {
+    	$_ = &header($_);
     }
-    elsif (($_ =~ /##/) && !($_ =~ /###/)) {
-      $_ = &transformH2($_);
-    }
-    elsif (($_ =~ /#/) && !($_ =~ /##/)) {
-      $_ = &transformH1($_);
-    }
+    
     #}
 
     print FILE2 $_."\n"; # write current line to file
@@ -29,21 +39,13 @@ sub process {
 }
 
 sub header {
-	
-}
-
-# code requirement
-
-sub list {
-
-}
-
-sub text {
-
-}
-
-sub links {
-
+	if ($_ =~ /#{4,}/) {
+		print "match #### or > \n";
+		$_ =~ s/#{4,} /<h3>/;
+		$_ =~ s/\h/<\/h3>/; # need way to find second occurance of space
+		print $_;
+		return $_;
+	}
 }
 
 sub transformH1 { #catch 1 hash symbol
@@ -64,13 +66,29 @@ sub transformH3 { # catch all with > 3 hash symbols
   return $_;
 }
 
-# main section starts here
+# code requirement
+
+sub list {
+
+}
+
+sub text {
+
+}
+
+sub links {
+
+}
+
+############################
+# main section starts here #
+############################
 
 # Getting input arguments
 my $input = $ARGV[0]; # CONSIDER CHECKING FOR MORE THAN TWO ARGUMENTS FOR BONUS MARKS
 my $output = $ARGV[1]; # might need to check if file exists
 
-print "number of items in ARGV is: ".scalar @ARGV."\n";
+#print "number of items in ARGV is: ".scalar @ARGV."\n";
 
 # Open input and output files
 open FILE1, "<", $input or die "Could not open input file\n";
